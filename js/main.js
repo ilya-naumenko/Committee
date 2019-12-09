ea01e96c7199395c10d675e91260f21580a611a2
ilya-naumenko <naumkoid@gmail.com>
2019-12-09 07:39:20 +0300

    ver 4

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 $(document).ready(function () {
 
-  $('.news-box').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3500
+
+
+});
+$(function () {
+  var location = window.location.href;
+  var cur_url = '/' + location.split('/').pop();
+
+  $('.nav li').each(function () {
+    var link = $(this).find('a').attr('href');
+
+    if (cur_url == link) {
+      $(this).addClass('current');
+    }
   });
-
-  $('.gallery-box').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
-
-  $('.comments-box').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3500
-  });
-
-  $('.popup-link, .button-form').magnificPopup();
-
 });
