07b6e10bcb4f9f64cb12d1106e9a62e18a952f3d
ilya-naumenko <naumkoid@gmail.com>
2019-12-09 09:00:27 +0300

    ver 5

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 });
 $(function () {
   var location = window.location.href;
-  var cur_url = '/' + location.split('/').pop();
+  var cur_url = '../' + location.split('/').pop();
 
   $('.nav li').each(function () {
     var link = $(this).find('a').attr('href');
